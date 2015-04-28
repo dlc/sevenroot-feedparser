f0dcfc88b98f0b3b12c15f1886e70127783c1942
Darren Chamberlain <dlc@sevenroot.org>
2015-04-27 22:24:02 -0700

    Beginnings of Atom syndication feed parser, and a simple test.

--- lib/Sevenroot/FeedParser/Atom.pm
+++ lib/Sevenroot/FeedParser/Atom.pm
@@ -2,7 +2,7 @@ package Sevenroot::FeedParser::Atom;
 
 use strict;
 use vars qw($VERSION);
-use Sevenroot::FeedParser::Util;
+use Sevenroot::FeedParser::Utils;
 
 $VERSION = "0.2e";
 
@@ -22,8 +22,91 @@ sub parse {
     $feed{'meta'}->{'source'} = $source;
     $feed{'meta'}->{'namespaces'} = extract_namespaces(\$data);
     $feed{'meta'}->{'xml_attrs'} = extract_xml_attrs(\$data);
+    $feed{'entries'} = $class->extract_items(\$data);
+    $feed{'channel'} = $class->extract_channel(\$data);
 
     return \%feed;
+}
+
+sub extract_items {
+    my $class = shift;
+    my $data = shift;
+    my @entries = ();
+
+    while ($$data =~ s!<entry>(.+?)</entry>!!s) {
+        push @entries, { raw => "$1" };
+    }
+
+    return \@entries;
+}
+
+sub extract_channel {
+    my $class = shift;
+    my $data = shift;
+    my %channel;
+
+#warn "data = '$$data'\n";
+
+    # Extract basic fields, based on http://atomenabled.org/developers/syndication/
+    for my $field (qw(
+        id title updated
+        icon logo rights subtitle
+        generator
+    )) {
+        if ($$data =~ s!<$field>(.+?)</$field>!!s) {
+            ($channel{ lc $field }) = unescape(trim("$1"));
+        }
+        elsif ($$data =~ s!<$field\s+.+?>(.+?)</$field>!!s) {
+            ($channel{ lc $field }) = unescape(trim("$1"));
+        }
+        else {
+            $channel{ lc $field } = "";
+        }
+    }
+
+    # Links
+    $channel{'links'} = {};
+    while ($$data =~ s!<(link\s+.+?)/>!!s) {
+        my $bits = extract_attributes("$1",
+            qw(href rel type hreflang title length));
+        $channel{'links'}->{ $bits->{'rel'} } = $bits;
+    }
+    if ($channel{'links'}->{'alternate'}) {
+        $channel{'link'} = $channel{'links'}->{'alternate'}->{'href'};
+    }
+
+    # Categories
+    $channel{'categories'} = [];
+    while ($$data =~ s!<(category\s+.+?)/>!!s) {
+        my $bits = extract_attributes("$1", qw(term scheme label));
+        push @{ $channel{'categories'} }, $bits;
+    }
+
+    # Contributor
+    $channel{'contributors'} = [];
+    while ($$data =~ s!<contributor>(.+?)</contributor>!!s) {
+        my $contrib = "$1";
+        my %contrib;
+        for my $field (qw(name uri email)) {
+            if ($contrib =~ s!<$field>(.+?)</$field>!!s) {
+                ($contrib{ $field }) = unescape(trim("$1"));
+            }
+            else {
+                $contrib{ $field } = "";
+            }
+        }
+        
+        push @{ $channel{'contributors'} }, \%contrib;
+    }
+
+    # Normalize date fields
+    for my $date_field (qw(updated)) {
+        if (my $date_str = delete $channel{$date_field}) {
+            $channel{ lc $date_field } = normalize_date($date_str);
+        }
+    }
+
+    return \%channel;
 }
 
 1;
--- t/stuff-you-should-know.atom.t
+++ t/stuff-you-should-know.atom.t
@@ -5,7 +5,7 @@ use strict;
 use File::Spec::Functions qw(catfile);
 use File::Basename qw(basename);
 use FindBin qw($Bin);
-use Test::More tests => 3;
+use Test::More tests => 7;
 
 # Disable DEBUG, and silence warnings
 $Sevenroot::FeedParser::DEBUG = $Sevenroot::FeedParser::DEBUG = 0;
@@ -16,3 +16,23 @@ ok(-f $datafile, "datafile exists");
 
 my $feed = parsefile($datafile);
 ok($feed, "Parsed datafile correctly");
+
+my $title = $feed->{'channel'}->{'title'};
+is($title,
+   "Stuff You Should Know: The Podcast",
+   "Parsed title");
+
+my $link = $feed->{'channel'}->{'link'};
+is($link,
+   "http://www.stuffyoushouldknow.com/stuffyoushouldknow-podcasts",
+   "Parsed link");
+
+my $generator = $feed->{'channel'}->{'generator'};
+is($generator,
+   "WordPress",
+   "Parsed generator");
+
+my $entries = $feed->{'entries'};
+is(scalar @$entries,
+   10,
+   "Parsed correct number of entries");
